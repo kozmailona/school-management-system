97f44938e94a8541481591287e343b3726fddf3d
kozmailona <kozmailona12@gmail.com>
2020-09-15 01:33:02 +0300

    added logger in userdetails service

--- src/main/java/org/fasttrackit/schoolmanagementsystem/service/UserDetailService.java
+++ src/main/java/org/fasttrackit/schoolmanagementsystem/service/UserDetailService.java
@@ -4,12 +4,15 @@ import org.apache.catalina.User;
 import org.fasttrackit.schoolmanagementsystem.domain.UserDetail;
 import org.fasttrackit.schoolmanagementsystem.persistence.UserDetailRepository;
 import org.fasttrackit.schoolmanagementsystem.transfer.SaveUserDetailRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetail.class);
     private final UserDetailRepository userDetailRepository;
 
     @Autowired
@@ -18,8 +21,8 @@ public class UserDetailService {
     }
 
     public User createUser(SaveUserDetailRequest request) {
-        System.out.println("Creating user " + request);
 
+        LOGGER.info("Creating user {}", request);
         UserDetail userDetail = new UserDetail();
         userDetail.setName(request.getName());
         userDetail.setAge(request.getAge());
@@ -29,7 +32,7 @@ public class UserDetailService {
         userDetail.setImageUrl(request.getImageUrl());
         userDetail.setPhoneNumber(request.getPhoneNumber());
         userDetail.setRegistrationNumber(request.getRegistrationNumber());
-//
+
         return (User) userDetailRepository.save(userDetail);
     }
 
